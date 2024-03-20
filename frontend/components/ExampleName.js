446868dcba263dde9d55c7b00d8539fe142fec9c
ViktorijaM1 <viktorijamickovskaaa@gmail.com>
2024-03-20 15:33:06 +0100

    Second commit

--- frontend/components/ExampleName.js
+++ frontend/components/ExampleName.js
@@ -1,5 +1,6 @@
-import { React, axios, connect, elements, ExportableGrid, GenericForm, Modal, GridManager } from 'perun-core'
-const { ReactBootstrap, alertUser } = elements
+import { React, axios, connect, elements, GenericForm, Modal, GridManager } from 'perun-core'
+const { alertUser } = elements
+import Grid from './Type'
 import './style.css'
 
 
@@ -7,17 +8,16 @@ const ExampleName = ({ session }) => {
     const [data, setData] = React.useState([]);
     const [selectedItem, setSelectedItem] = React.useState();
     const [selectedSubItem, setSelectedSubItem] = React.useState(null);
-    const [show, setShow] = React.useState(false);
     const [modal, setModal] = React.useState(undefined);
     const [table_, setTableName] = React.useState(null);
-    const [objectId, setObjectId] = React.useState(0)
     const [openSubmenus, setOpenSubmenus] = React.useState([]);
-    const [readOnly, setReadOnly] = React.useState(null)
+    const [readOnly, setReadOnly] = React.useState(null);
+
 
     const formId = table_ + '_FORM'
     const gridId = table_ + '_GRID'
 
-
+    const hideBtns = !readOnly ? '' : 'all'
 
     const url = window.server + `/WsAdminConsole/get-configuration/sid/${session}/component-name/intern-test-menu`
     React.useEffect(() => {
@@ -29,12 +29,12 @@ const ExampleName = ({ session }) => {
                 console.error('Error fetching data:', error);
                 alertUser(true, error.data.type.toLowerCase(), error.data.title, error.data.message)
             });
-    }, [session]);
+    }, []);
 
     const handleItemClick = (item) => {
 
         setSelectedItem(item);
-
+        setReadOnly(item.objectConfiguration?.form?.configuration?.readOnly);
         if (item.data && item.data.length > 0) {
 
             setOpenSubmenus((prevOpenSubmenus) =>
@@ -42,16 +42,14 @@ const ExampleName = ({ session }) => {
                     ? prevOpenSubmenus.filter((id) => id !== item.ID)
                     : [...prevOpenSubmenus, item.ID]
             );
-
         }
         setSelectedSubItem(null);
 
-
     };
-
 
     const handleSubItemClick = (subItem) => {
         setSelectedSubItem(subItem);
+        setReadOnly(subItem.objectConfiguration?.form?.configuration?.readOnly);
 
     };
 
@@ -63,26 +61,35 @@ const ExampleName = ({ session }) => {
 
     const isSubmenuOpen = (item) => openSubmenus.includes(item.ID);
 
-    const generateForm = (objecidUser, table) => {
+    const showModal = (objectId) => {
+
+        const saveRecord = (e) => {
+
+            const url = window.server + `/ReactElements/createTableRecordFormData/${session}/${table_}/0`
+            axios({
+                method: 'post',
+                data: encodeURIComponent(JSON.stringify(e.formData)),
+                url,
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+
+            }).
+                then(res => {
+                    if (res.data) {
+                        const resType = res.data.type.toLowerCase()
+                        alertUser(true, resType, res.data.title)
 
 
-        const form = (<GenericForm
-            key={formId}
-            id={formId}
-            method={`/ReactElements/getTableJSONSchema/${session}/` + table}
-            uiSchemaConfigMethod={`/ReactElements/getTableUISchema/${session}/${table}`}
-            tableFormDataMethod={`/ReactElements/getTableFormData/${session}/${objecidUser}/${table}`}
-            addSaveFunction={(e) => saveRecord(e)}
-            addDeleteFunction={deleteRecord}
-        />
-        );
+                        if (resType === 'success')
+                            closeModal();
+                        GridManager.reloadGridData(gridId)
 
-        generateModal(form)
+                    }
+                }).catch(error => {
+                    alertUser(true, res.data.type.toLowerCase(), "You cannot save records in this table")
+                    console.error('Error fetching data:', error);
 
-    }
-
-
-    const addRow = () => {
+                })
+        }
 
         const form = (<GenericForm
             key={formId}
@@ -92,42 +99,41 @@ const ExampleName = ({ session }) => {
             tableFormDataMethod={`/ReactElements/getTableFormData/${session}/${objectId}/${table_}`}
             addSaveFunction={(e) => saveRecord(e)}
             addDeleteFunction={deleteRecord}
+            hideBtns={hideBtns}
+            disabled={readOnly}
         />
+
         );
 
-        generateModal(form)
-
-    }
-
-    const generateModal = (form) => {
-
-        setShow(true);
         const modal = (
 
             <Modal
-                show={show}
                 id={selectedItem.ID}
                 key={selectedItem.ID}
                 modalContent={form}
                 closeModal={closeModal}
-                onHide={() => { setShow(false) }}
+                closeAction={closeModal}
 
             />
         );
         setModal(modal);
-
     }
 
 
     const onRowClick = (_gridId, _rowId, row) => {
-        setShow(true);
+
         const tableName = getTableNameFromId(selectedItem);
         const objecidUser = row[`${tableName}.OBJECT_ID`];
-        setObjectId(objecidUser);
-        generateForm(objecidUser, tableName);
-
+        setTableName(tableName);
+        showModal(objecidUser)
 
     }
+
+    const closeModal = () => {
+        setModal(false)
+
+    }
+
 
     React.useEffect(() => {
         if (selectedItem) {
@@ -138,11 +144,7 @@ const ExampleName = ({ session }) => {
 
 
     const deleteRecord = (gridId_, tableName, session_, params) => {
-        if (readOnly) {
-            alertUser(true, 'error', "You cannot save records ");
-            setShow(false);
-            return;
-        }
+
 
         const url = window.server + `/ReactElements/deleteObject/${session}`
         const formData = params[4].PARAM_VALUE
@@ -160,7 +162,7 @@ const ExampleName = ({ session }) => {
                 if (res.data) {
 
                     alertUser(true, res.data.type.toLowerCase(), "Record deleted")
-                    setShow(false);
+                    closeModal();
                     GridManager.reloadGridData(gridId)
                 }
 
@@ -170,147 +172,58 @@ const ExampleName = ({ session }) => {
 
             })
     }
-    const saveRecord = (e) => {
 
-        if (readOnly) {
-            alertUser(true, 'error', "You cannot save records ");
-            setShow(false);
-            return;
-        }
-
-        const url = window.server + `/ReactElements/createTableRecordFormData/${session}/${table_}/0`
-        axios({
-            method: 'post',
-            data: encodeURIComponent(JSON.stringify(e.formData)),
-            url,
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-
-        }).
-            then(res => {
-                if (res.data) {
-                    const resType = res.data.type.toLowerCase()
-                    alertUser(true, resType, res.data.title)
-                    if (resType === 'success')
-                        setShow(false);
-                    GridManager.reloadGridData(gridId)
-
-                }
-            }).catch(error => {
-                alertUser(true, res.data.type.toLowerCase(), "You cannot save records in this table")
-                console.error('Error fetching data:', error);
-
-            })
-    }
-
-    const closeModal = () => {
-        GridManager.reloadGridData(gridId)
-        setShow(false);
-
-    };
 
     const getTableNameFromId = (selectedItem) => {
         return selectedItem.ID.replace(/_\d+$/, '');
 
     };
 
-
-    const typeGrid = (item) => {
-        return item && item.objectConfiguration && item.objectConfiguration.type === 'grid';
-    };
-    const Grid = ({ item }) => {
-        if (typeGrid(item)) {
-            const readOnly = item?.objectConfiguration?.form?.configuration?.readOnly;
-
-            if (readOnly === true) {
-                setReadOnly(true);
-            }
-
-            return (
-                <ExportableGrid
-                    id={gridId}
-                    key={gridId}
-                    configTableName={item.objectConfiguration.configuration?.onSubmit}
-                    dataTableName={item.objectConfiguration.data?.onSubmit}
-                    onRowClickFunct={onRowClick}
-                    toggleCustomButton={!readOnly && true}
-                    customButton={addRow}
-                    customButtonLabel='Add'
-                />
-            );
-        } else if (selectedItem && selectedItem.objectConfiguration && selectedItem.objectConfiguration.type === 'form') {
-            return (
-                <>
-                    <GenericForm
-                        key={gridId}
-                        id={gridId}
-                        method={selectedItem.objectConfiguration.configuration?.onSubmit}
-                        uiSchemaConfigMethod={selectedItem.objectConfiguration.uischema?.onSubmit}
-                        tableFormDataMethod={selectedItem.objectConfiguration.data?.onSubmit}
-                    />
-                </>
-            );
-        } else {
-            return null;
-        }
-    };
-
-
     return (
         <>
-            <div className='flex-parent parent-margin'>
-                <div className='sidebar'>
-                    <div className='flex-column'>
-                        <div className='flex-parent admin-console-side-bar-content'>
-                            <div className='nav-menu'>
-                                return (
-                                <ul className='menu-list'>
-                                    {data.map((item) => (
-                                        <>
-                                            <li
-                                                key={item.ID}
-                                                className={item.data ? 'var_nav_arrow' : 'var_nav'}
-                                                onClick={() => handleItemClick(item)}
-                                                tabIndex='1'
-                                            >
-                                                <div className={`link_bg`}></div>
-                                                <div className='title_'>
-                                                    <div class='menu-links' >
-                                                        {item.label}
-                                                    </div>
-                                                </div>
-                                            </li>
-                                            {isSubmenuOpen(item) && (
-                                                <div>
-                                                    {item.data.map((subItem) => (
-                                                        <div
-                                                            key={subItem.ID}
-                                                            onClick={() => handleSubItemClick(subItem)}
-                                                            className={`submenu ${selectedSubItem === subItem ? 'active' : ''}`}
-                                                        >
-                                                            <div className='subItem-label' >{subItem.label} </div>
-                                                        </div>
-                                                    ))}
-                                                </div>
-                                            )}
-                                        </>
-                                    ))}
-                                </ul>
-                                );
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="content">
-                    <div className='admin-console-grid-container'>
-                        <Grid item={typeGrid(selectedSubItem) ? selectedSubItem : selectedItem} />
+            <nav className="sidebar-menu">
+                <ul className="navbar-nav mr-auto">
+                    {data.map((item) => (
+                        <li className="nav-item" key={item.id}>
+                            <a
+                                className={`nav-link ${item.data && item.data.length > 0 ? 'dropdown-toggle' : ''}`}
+                                onClick={() => handleItemClick(item)}
+                            >
+                                {item.label}
 
-                        {selectedItem && show && modal || selectedSubItem && show && modal}
-                    </div>
-                </div>
-            </div >
+                            </a>
+                            {isSubmenuOpen(item) && (
+                                <div className="dropdown-menu" aria-labelledby={`navbarDropdown${item.id}`}>
+                                    {item.data.map((subItem) => (
+                                        <a key={subItem.id} className="dropdown-item" onClick={() => handleSubItemClick(subItem)}>
+                                            {subItem.label}
+                                        </a>
+                                    ))}
+                                </div>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+
+
+            <div className='container grid-container'>
+
+                <Grid
+                    item={selectedSubItem ? selectedSubItem : selectedItem}
+                    showModal={showModal}
+                    readOnly={readOnly}
+                    gridId={gridId}
+                    onRowClick={onRowClick}
+                />
+
+                {selectedItem && modal}
+            </div>
+
         </>
     );
 }
+
 
 const mapStateToProps = (state) => ({
     session: state.security.svSession
---
+++ frontend/components/Type.js
@@ -0,0 +1,38 @@
+import { React, ExportableGrid, GenericForm } from 'perun-core'
+
+const Grid = ({ item, showModal, readOnly, gridId, onRowClick }) => {
+
+
+    if (item && item.objectConfiguration && item.objectConfiguration.type === 'grid') {
+
+        return (
+            <ExportableGrid
+                id={gridId}
+                key={gridId}
+                configTableName={item.objectConfiguration?.configuration?.onSubmit}
+                dataTableName={item.objectConfiguration?.data?.onSubmit}
+                onRowClickFunct={onRowClick}
+                toggleCustomButton={!readOnly}
+                customButton={showModal}
+                customButtonLabel='Add'
+            />
+        );
+
+    } else if (item && item.objectConfiguration && item.objectConfiguration.type === 'form') {
+        return (
+            <>
+                <GenericForm
+                    key={gridId}
+                    id={gridId}
+                    method={item.objectConfiguration.configuration?.onSubmit}
+                    uiSchemaConfigMethod={item.objectConfiguration.uischema?.onSubmit}
+                    tableFormDataMethod={item.objectConfiguration.data?.onSubmit}
+                />
+            </>
+        );
+    } else {
+        return null;
+    }
+};
+
+export default Grid;
